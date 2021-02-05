d99884ed2012ca896dba9dc3ffee5c0a9a4d23ab
npinkam <n.pinkam@lexer.co.jp>
2021-02-05 13:55:07 +0900

    add httpNodeCors in node-red settings

--- index.js
+++ index.js
@@ -29,7 +29,11 @@ var settings = {
     userDir: '',
     functionGlobalContext: {
         authorization: ''
-    }    // enables global context
+    },    // enables global context
+    httpNodeCors: {
+        origin: "*",
+        methods: "GET,PUT,POST,DELETE"
+    },
 };
 
 // Create an Express app
@@ -114,7 +118,11 @@ app.post('/auth', (req, res) => {
                 userDir: `./userDir/${username}`,
                 functionGlobalContext: {
                     authorization: authorization
-                }    // enables global context
+                },    // enables global context
+                httpNodeCors: {
+                    origin: "*",
+                    methods: "GET,PUT,POST,DELETE"
+                },
             };
 
             // Initialise the runtime with a server and settings
@@ -179,12 +187,12 @@ app.get('/projectlist', (req, res) => {
         var style = '';
         var header = ``;
 
-/*         response.forEach(element => {
-            var buffer = element.id;
-            var link = `/projinfo?projectId=${buffer}`;
-            element.id = "<a href=" + link + " target='_self'>" + buffer + "</a>";
-
-        }); */
+        /*         response.forEach(element => {
+                    var buffer = element.id;
+                    var link = `/projinfo?projectId=${buffer}`;
+                    element.id = "<a href=" + link + " target='_self'>" + buffer + "</a>";
+        
+                }); */
 
         var body = `
 <div class="container">
@@ -225,22 +233,22 @@ app.get('/pvdolist', (req, res) => {
         xhr.send();
         var response = JSON.parse(xhr.responseText);
 
-/*         //current mining
-        var buffer = response.miningmanager_status.CurrentMiningID;
-        var link = `/PVDOinfo?MiningID=${buffer}`;
-        response.miningmanager_status.CurrentMiningID = "<a href=" + link + " target='_self'>" + buffer + "</a>";
- */
-/*         response.mining_statuses.forEach(element => {
-            var buffer = element.MiningID;
-            var link = `/PVDOinfo?MiningID=${buffer}`;
-            element.MiningID = "<a href=" + link + " target='_self'>" + buffer + "</a>";
-
-            if (element.Status != 'Complete' && element.Status != 'Cancel') {
-                var bufferStatus = element.Status;
-                link = `/PVDOabort?MiningID=${buffer}`;
-                element.Status = `<p>${bufferStatus} (</p><a href=${link} target='_self'>ABORT</a><p>)</p>`;
-            }
-        }); */
+        /*         //current mining
+                var buffer = response.miningmanager_status.CurrentMiningID;
+                var link = `/PVDOinfo?MiningID=${buffer}`;
+                response.miningmanager_status.CurrentMiningID = "<a href=" + link + " target='_self'>" + buffer + "</a>";
+         */
+        /*         response.mining_statuses.forEach(element => {
+                    var buffer = element.MiningID;
+                    var link = `/PVDOinfo?MiningID=${buffer}`;
+                    element.MiningID = "<a href=" + link + " target='_self'>" + buffer + "</a>";
+        
+                    if (element.Status != 'Complete' && element.Status != 'Cancel') {
+                        var bufferStatus = element.Status;
+                        link = `/PVDOabort?MiningID=${buffer}`;
+                        element.Status = `<p>${bufferStatus} (</p><a href=${link} target='_self'>ABORT</a><p>)</p>`;
+                    }
+                }); */
 
         var title = 'GD.findi PVDO List'
         var library = `
